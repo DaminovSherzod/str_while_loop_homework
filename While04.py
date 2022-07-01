b82cc27de26ec2b68bde76e3695cd6b2d72bcba2
Sherzod <daminovsherzod2002@gmail.com>
2022-07-01 14:48:04 +0500

    While04.py

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    x=0
+    while a<len(s):
+        if s[a].isupper():
+            x+=1
+        a+=1    
+    return x
+print(main('CodeschoolUz'))    
