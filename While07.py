1dfaf1afeb8d626994c74ecca57875ecfbaaff8e
Sherzod <daminovsherzod2002@gmail.com>
2022-07-01 15:52:29 +0500

    While07.py

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    x=0
+    while a<len(s):
+        if s[a].isdigit() and s[a]!='1' and s[a]!='3' and s[a]!='5' and s[a]!='7' and s[a]!='9':
+            x+=1
+        a+=1    
+    return x
+print(main('123456'))    
