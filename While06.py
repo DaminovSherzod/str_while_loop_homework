ad0112a769c645ad400577a4e3123b1ecc18c399
Sherzod <daminovsherzod2002@gmail.com>
2022-07-01 15:51:45 +0500

    While06.py

--- While06.py
+++ While06.py
@@ -7,3 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    a = 0
+    x = 0
+    while a<len(s):
+        if s[a]!='a' and s[a]!='e' and s[a]!='i' and s[a]!='o' and s[a]!='A' and s[a]!='E' and s[a]!='I' and s[a]!='O' and s[a]!='u' and s[a]!='U' and s[a].isalpha():
+            x+=1
+        a+=1
+    return x
+print(main('COdeSchooluz'))            
